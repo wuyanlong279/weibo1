4d5e4b0fd2acace2d673a870b8565d9affee15c5
wuhaobo <414450418@qq.com>
2019-12-05 15:07:32 +0000

    help

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title', 'weibo app')--学习模板</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'WeiBo')
+@section('content')
+<h1>主页</h1>
+@stop
