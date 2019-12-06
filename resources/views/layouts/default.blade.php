6e16704351c2357410872313a1776d12653f6e74
wuhaobo <414450418@qq.com>
2019-12-06 04:12:43 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', 'weibo app')--学习模板</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
